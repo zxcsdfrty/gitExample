27852df8e56e6eeda860c84cb4754cfd9ad7e0b2
許震 <zxcsdfrty12345@gmail.com>
2019-04-24 23:48:52 +0800

    newfile Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+#master
+
+#示範Git用的程式碼！
+
+def add(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def times(a, b):
+    return a * b
+
+def divided(a, b):
+    if b != 0:
+        return a / b
+    return "無限"
